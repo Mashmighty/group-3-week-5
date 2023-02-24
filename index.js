4c9d25169d83d12c1e777be354e17d715f44b699
Mashmighty <georgesm0101@gmail.com>
2023-02-24 14:05:26 +0300

    error setup port 3030

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 //create an express app
 const app = express();
 
-app.listen(3000, {} => {
-    
+app.listen(3030, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", 3030);
 })
